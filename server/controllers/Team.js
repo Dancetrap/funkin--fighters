ba01774fc2a2c7ad8826b7e27dc4090e538e5448
Dancetrap <85971084+Dancetrap@users.noreply.github.com>
2022-11-11 12:45:22 -0500

    get team

--- server/controllers/Team.js
+++ server/controllers/Team.js
@@ -1,6 +1,19 @@
+const models = require('../models');
+const TeamModel = require('../models/Domo');
+
+const { Team } = models;
+
 const makerPage = (req, res) => res.render('team');
+
+const getTeam = (req, res) => TeamModel.findByOwner(req.session.account._id, (err, docs) => {
+  if (err) {
+    console.log(err);
+    return res.status(400).json({ error: 'An error occurred!' });
+  }
+  return res.json({ team: docs });
+});
 
 module.exports = {
   makerPage,
-  //   findCharacters,
+  getTeam,
 };
